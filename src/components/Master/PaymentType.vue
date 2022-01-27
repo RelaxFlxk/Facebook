c4602fd9cd1e5b31ea2b313e53c9ce1e8446dd93
chinnawut.vgrouphonda <chinnawut.vgrouphonda@gmail.com>
2022-01-27 12:07:15 +0700

    update image

--- src/components/Master/PaymentType.vue
+++ src/components/Master/PaymentType.vue
@@ -8,7 +8,7 @@
 
           <!-- Dialog export / import -->
           <v-col cols="12">
-            <v-btn color="primary" depressed @click="dialogAdd = true">
+            <v-btn color="primary" depressed @click="dialogAdd = true, validate('ADD')">
               <v-icon left>mdi-text-box-plus</v-icon>
               Add
             </v-btn>
@@ -27,11 +27,35 @@
               </center>
               <center>
                 <v-col>
-                  <v-img class="v_text_add" :src="require('@/assets/Grouptitle.svg')"></v-img>
+                  <!-- <v-img class="v_text_add" :src="require('@/assets/Grouptitle.svg')"></v-img> -->
+                  <h4 style="font-size:7vw;" class="underline-06">เพิ่ม ช่องทางการชำระเงิน</h4>
                 </v-col>
               </center>
+              <v-form ref="form_add" v-model="validAdd" lazy-validation>
               <v-card-text>
                 <v-container>
+                  <v-row justify="center">
+                    <v-col cols="12" class="text-center">
+                      <v-img
+                        aspect-ratio="6"
+                        contain
+                        :src="formAdd.pictureUrlPreview"
+                      ></v-img>
+                      <!-- <v-avatar size="100px"><img alt="Avatar" :src="formAdd.pictureUrl"></v-avatar> -->
+                      <br />
+                      <v-file-input
+                        required
+                        counter
+                        show-size
+                        :rules="[rules.resizeImag]"
+                        accept="image/png, image/jpeg, image/bmp"
+                        prepend-icon="mdi-camera"
+                        label="Image"
+                        @change="selectImgAdd"
+                        v-model="filesAdd"
+                      ></v-file-input>
+                    </v-col>
+                  </v-row>
                   <v-row>
                     <v-container fluid>
                       <v-col>
@@ -41,12 +65,14 @@
                       </v-col>
                       <v-textarea
                          rows="3" row-height="25"
+                         :rules="[rules.required]"
                         v-model="formAdd.payTypeName"
                       ></v-textarea>
                     </v-container>
                   </v-row>
                 </v-container>
               </v-card-text>
+              </v-form>
 
                 <center>
                   <v-col class="text-center">
@@ -54,11 +80,22 @@
                   elevation="2"
                   x-large
                   dark
+                  :disabled="!validAdd"
                   color="#1B437C"
                   @click="addData()"
                 >
                   <v-icon left>mdi-checkbox-marked-circle</v-icon>
                   เพิ่ม
+                </v-btn>
+                <v-btn
+                  elevation="2"
+                  x-large
+                  dark
+                  color="red"
+                  @click="(dialogAdd = false), clearData()"
+                >
+                  <v-icon left>mdi-checkbox-marked-circle</v-icon>
+                  ปิด
                 </v-btn>
                   </v-col>
                 </center>
@@ -79,11 +116,33 @@
               </center>
               <center>
                 <v-col>
-                  <v-img id="v_text_edits" :src="require('@/assets/GroupEditTitle.svg')"></v-img>
+                  <h4 style="font-size:7vw;" class="underline-06">แก้ไข ช่องทางการชำระเงิน</h4>
                 </v-col>
               </center>
               <v-card-text>
                 <v-container>
+                  <v-row justify="center">
+                    <v-col cols="12" class="text-center">
+                      <v-img
+                        aspect-ratio="6"
+                        contain
+                        :src="formUpdate.pictureUrlPreview"
+                      ></v-img>
+                      <!-- <v-avatar size="100px"><img alt="Avatar" :src="formAdd.pictureUrl"></v-avatar> -->
+                      <br />
+                      <v-file-input
+                        required
+                        counter
+                        show-size
+                        :rules="[rules.resizeImag]"
+                        accept="image/png, image/jpeg, image/bmp"
+                        prepend-icon="mdi-camera"
+                        label="Image"
+                        @change="selectImgUpdate"
+                        v-model="filesUpdate"
+                      ></v-file-input>
+                    </v-col>
+                  </v-row>
                   <v-row>
                     <v-container fluid>
                       <v-col>
@@ -198,6 +257,11 @@
                   :search="searchAll2"
                   :items-per-page="10"
                 >
+                  <template v-slot:[`item.payTypeImage`]="{ item }">
+                    <v-avatar color="primary" size="40">
+                      <img :src="item.payTypeImage" alt="img"
+                    /></v-avatar>
+                  </template>
                    <template v-slot:[`item.CREATE_DATE`]="{ item }">
                       {{ format_date(item.CREATE_DATE) }}
                   </template>
@@ -299,28 +363,44 @@ export default {
       formAdd: {
         payTypeCode: '',
         payTypeName: '',
-        shopId: this.$session.getAll().data.shopId
+        shopId: this.$session.getAll().data.shopId,
+        pictureUrlPreview: '',
+        payTypeImage: ''
       },
       formUpdate: {
         payTypeCode: '',
-        payTypeName: ''
+        payTypeName: '',
+        pictureUrlPreview: '',
+        payTypeImage: ''
       },
       formUpdateItem: {
         payTypeCode: '',
-        payTypeName: ''
+        payTypeName: '',
+        payTypeImage: ''
       },
-      nameRules: [
-        (v) => !!v || 'Name is required',
-        (v) => v.length <= 50 || 'Name must be less than 50 characters'
-      ],
-      nameSelect: [
-        (v) => !!v || 'select is required',
-        (v) => v.length > 1 || 'Please select'
-      ],
+      rules: {
+        numberRules: value =>
+          (!isNaN(parseFloat(value)) && value >= 0 && value <= 9999999999) ||
+          'กรุณากรอกตัวเลข 0 ถึง 9',
+        counterTel: value => value.length <= 10 || 'Max 10 characters',
+        IDcardRules: value =>
+          (!isNaN(parseFloat(value)) && value >= 0 && value <= 9999999999999) ||
+          'กรุณากรอกตัวเลข 0 ถึง 9',
+        required: value => !!value || 'กรุณากรอก.',
+        resizeImag: value =>
+          !value ||
+          value.size < 2000000 ||
+          'Avatar size should be less than 2 MB!',
+        counterIDcard: value => value.length <= 13 || 'Max 13 characters',
+        email: value => {
+          const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+          return pattern.test(value) || 'Invalid e-mail.'
+        }
+      },
       // End Form Config ADD EDIT
       // Data Table Config
       columns: [
-        { text: 'ID', value: 'payTypeId' },
+        { text: 'รูปภาพ', value: 'payTypeImage' },
         { text: 'รหัส', value: 'payTypeCode' },
         { text: 'ประเภทการชำระเงิน', value: 'payTypeName' },
         { text: 'วันที่สร้าง', value: 'CREATE_DATE' },
@@ -336,7 +416,11 @@ export default {
             value: 'utf-8'
           }
         ]
-      ]
+      ],
+      filesAdd: null,
+      filesUpdate: null,
+      validAdd: true,
+      validUpdate: true
     }
   },
   async mounted () {
@@ -346,6 +430,45 @@ export default {
     this.getDataGlobal(this.DNS_IP, this.path, this.$session.getAll().data.shopId)
   },
   methods: {
+    validate (Action) {
+      switch (Action) {
+        case 'ADD':
+          this.$nextTick(() => {
+            let self = this
+            self.$refs.form_add.validate()
+          })
+          break
+        case 'UPDATE':
+          this.$nextTick(() => {
+            let self = this
+            self.$refs.form_update.validate()
+          })
+          break
+
+        default:
+          break
+      }
+    },
+    selectImgAdd () {
+      if (this.filesAdd) {
+        this.formAdd.pictureUrlPreview = URL.createObjectURL(
+          this.filesAdd
+        )
+      } else {
+        this.formAdd.pictureUrlPreview = ''
+      }
+      // console.log(event)
+    },
+    selectImgUpdate () {
+      if (this.filesUpdate) {
+        this.formUpdate.pictureUrlPreview = URL.createObjectURL(
+          this.filesUpdate
+        )
+      } else {
+        this.formUpdate.pictureUrlPreview = ''
+      }
+      // console.log(event)
+    },
     async getDataById (item) {
       //
       //
@@ -355,6 +478,7 @@ export default {
       // Get ID /main.js
       this.dataReady = false
       this.getDataByIdGlobal(this.DNS_IP, this.path, 'payTypeId', item.payTypeId)
+      this.formUpdate.pictureUrlPreview = item.payTypeImage
     },
     async addData () {
       //
@@ -385,6 +509,20 @@ export default {
         cancelButtonText: 'ไม่'
       })
         .then(async (result) => {
+          if (this.filesAdd) {
+            const _this = this
+            let params = new FormData()
+            params.append('file', this.filesAdd)
+            await axios
+              .post(this.DNS_IP + `/file/upload/payment`, params)
+              .then(function (response) {
+                _this.formAdd.payTypeImage = response.data
+                console.log('url Pic', response.data)
+              })
+          } else {
+            this.formAdd.payTypeImage = this.formAdd.pictureUrlPreview
+          }
+          delete this.formAdd['pictureUrlPreview']
           await axios
             .post(
               // eslint-disable-next-line quotes
@@ -431,6 +569,20 @@ export default {
           }
         }
       }
+      if (this.filesUpdate) {
+        const _this = this
+        let params = new FormData()
+        params.append('file', this.filesUpdate)
+        await axios
+          .post(this.DNS_IP + `/file/upload/shop`, params)
+          .then(function (response) {
+            _this.formUpdateItem.payTypeImage = response.data
+            console.log('url Pic', response.data)
+          })
+      } else {
+        this.formUpdateItem.payTypeImage = this.formUpdate.pictureUrlPreview
+      }
+      // delete this.formAdd['pictureUrlPreview']
       console.log('this.formUpdateItem', this.formUpdateItem)
 
       // Debug
