8653edb28f986a24920af15c05d2390455a45c26
chinnawut.vgrouphonda <chinnawut.vgrouphonda@gmail.com>
2022-02-01 11:30:22 +0700

    update calenda new version แต่ยังไม่ได้ deploy นะครับ

--- src/components/Master/CalendarBookingList.vue
+++ src/components/Master/CalendarBookingList.vue
@@ -16,7 +16,6 @@
                   label="วันที่"
                   prepend-icon="mdi-calendar"
                   readonly
-                  outlined
                   v-bind="attrs"
                   v-on="on"
                 ></v-text-field>
@@ -42,14 +41,29 @@
             >
             </v-select>
           </v-col>
+          <v-col cols="4">
+            <v-select
+              :items="DataBranchName"
+              v-model="masBranchName"
+              @change="getBookingList()"
+              dense
+              outlined
+              filled
+              hide-details
+              item-text="text"
+              item-value="value"
+              return-object
+              label="สาขา"
+              prepend-inner-icon="mdi-map-marker"
+              class="ma-2"
+            ></v-select>
+          </v-col>
           <v-col cols="12">
             <v-alert
               color="primary"
               dark
-              size="56"
               icon="mdi-account-clock"
               border="left"
-              prominent
             >
               จำนวนลูกค้าต่อวัน : {{ countCus || 0 }}
             </v-alert>
@@ -225,6 +239,22 @@
                     รายชื่อลูกค้านัดหมาย
                   </v-card-title>
                   <br />
+                  <template v-for="(items, index) in dataSummary">
+                    <v-chip
+                      class="ma-2"
+                      color="grey"
+                      text-color="white"
+                      v-bind:key="'sum'+index"
+                    >
+                      <v-avatar
+                        left
+                        class="grey darken-3"
+                      >
+                        {{items.length}}
+                      </v-avatar>
+                      {{index}}
+                    </v-chip>
+                  </template>
                   <v-card-text
                     v-for="(items, index) in dataCalendar"
                     :key="index"
@@ -339,6 +369,7 @@ export default {
       menuDate: false,
       dialog: false,
       dataCalendar: [],
+      dataSummary: [],
       DataFlowName: [],
       colors: [
         'blue',
@@ -677,6 +708,7 @@ export default {
     },
     openTaskList (date, type) {
       console.log('start TaskList')
+      this.dataSummary = []
       this.dataCalendar = []
       let targetData = null
       if (type === 'fastTrack') {
@@ -690,7 +722,6 @@ export default {
       }
       for (let i = 0; i < targetData.length; i++) {
         let d = targetData[i]
-        console.log('d', d)
         d.chkConfirm = false
         d.chkCancel = false
         if (d.statusUseBt === 'use' && d.statusBt === 'confirm') {
@@ -718,7 +749,6 @@ export default {
         d.carModel = (d.carModel.length > 0) ? d.carModel[0].fieldValue : ''
         this.dataCalendar.push(d)
       }
-      console.log('dataCalendar', this.dataCalendar)
       this.dataCalendar.sort((a, b) => {
         let keyA = new Date(a.dueDate)
         let keyB = new Date(b.dueDate)
@@ -726,7 +756,13 @@ export default {
         if (keyA > keyB) return 1
         return 0
       })
-      console.log(this.dataCalendar)
+
+      this.dataSummary = this.dataCalendar.reduce(function (r, a) {
+        r[a.timeDue] = r[a.timeDue] || []
+        r[a.timeDue].push(a)
+        return r
+      }, Object.create(null))
+      console.log('dataSummary', this.dataSummary)
       this.dialog = true
     },
     async showEvent (event) {
