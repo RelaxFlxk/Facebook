e10b167987aa1ecbf844bc405e26709466940d82
Chinnawut Wongjino <chinnawut.vgrouphonda@gmail.com>
2022-11-30 15:47:35 +0700

    fix

--- src/components/Master/BookingField.vue
+++ src/components/Master/BookingField.vue
@@ -698,7 +698,7 @@
                       ></v-checkbox>
                       <v-text-field v-model="showTime" readonly label="แสดงเวลาการจองหรือไม่"></v-text-field>
                     </v-row>
-                    <v-row align="center">
+                    <!-- <v-row align="center">
                       <v-checkbox
                         false-value="False"
                         true-value="True"
@@ -707,7 +707,7 @@
                         class="shrink ml-6 mr-0 mt-0 mb-6"
                       ></v-checkbox>
                       <v-text-field :value="multiDueDate === 'True' ? 'ใช้งาน' : 'ไม่ใช้งาน'" readonly label="นัดหมายหลายวันหรือไม่"></v-text-field>
-                    </v-row>
+                    </v-row> -->
                     <!-- <v-row align="center">
                       <v-checkbox
                         false-value="False"
