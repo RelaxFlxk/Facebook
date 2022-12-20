10e722338992a64e36c298986a49f2fc8d0519e3
Chinnawut Wongjino <chinnawut.vgrouphonda@gmail.com>
2022-12-20 14:28:10 +0700

    fix

--- src/components/Master/BookingList.vue
+++ src/components/Master/BookingList.vue
@@ -1772,7 +1772,7 @@
                     append-icon="mdi-text-box-search"
                     label="ค้นหาทั้งหมด"
                     :color="showColorSearch ? 'green' : 'info'"
-                    @click:append="searchAny(), showColorSearch = true, statusSearch = 'yes', dataReady = false"
+                    @click:append="searchAny()"
                     outlined
                   ></v-text-field>
                 </v-col>
@@ -3753,6 +3753,9 @@ export default {
     },
     async searchAny () {
       if (this.searchOther.trim().length > 1) {
+        this.showColorSearch = true
+        this.statusSearch = 'yes'
+        this.dataReady = false
         // this.dataReady = false
         this.selectedStatus = true
         // this.getSelectText = ''
@@ -3922,6 +3925,9 @@ export default {
           //   this.$router.push('/system/Errorpage?returnLink=' + returnLink)
           })
       } else {
+        this.dataReady = true
+        this.showColorSearch = false
+        this.statusSearch = 'no'
         this.$swal('ผิดพลาด', 'กรุณาใส่คำค้นหาให้มากว่า 1 ตัวอักษร', 'error')
       }
     },
